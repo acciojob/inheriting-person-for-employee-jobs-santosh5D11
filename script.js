d7583f16055433eee3409ce8ef2d58ab02bf9843
Motapandle Santosh <santoshmotapandle@gmail.com>
2026-09-09 21:31:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class Person {
 	}
 
 	greet(){
-		return `Hello, my name is ${this.name}, I am ${this.age} years old`.
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`).
 	}
 }
 
@@ -17,8 +17,8 @@ class Employee extends Person {
 	}
 
 	jobGreet(){
-		return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title 
-		is ${this.jobTitle}`;
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title 
+		is ${this.jobTitle}`);
 	}
 }
 
