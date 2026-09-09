a4953a3c56df6334fd8217e9130382d3487b1889
Motapandle Santosh <santoshmotapandle@gmail.com>
2026-09-09 21:17:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	constructor(name,age){
+		this.name = name;
+		this.ag = age;
+	}
 
-function Employee(name, age, jobTitle) {}
+	greet(){
+		return `Hello, my name is ${this.name}, I am ${this.age} year old`.
+	}
+}
+
+function Employee(name, age, jobTitle) {
+	constructor(name,age,jobTitle){
+		super(name,age)
+		this.jobTitle = jobTitle;
+	}
+
+	jobGreet(){
+		return `Hello, my name is ${this.name}, I am ${this.age} year old, and my job title 
+		is ${this.jobTitle}`;
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
