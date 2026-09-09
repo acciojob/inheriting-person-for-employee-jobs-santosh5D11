1204dcf331b2086bb06fbe9ba121479e3a46b83b
Motapandle Santosh <santoshmotapandle@gmail.com>
2026-09-09 21:35:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 // complete this js code
 class Person {
-	constructor(name,age){
+	constructor(name, age) {
 		this.name = name;
-		this.ag = age;
+		this.age = age;
 	}
 
-	greet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`).
+	greet() {
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 	}
 }
 
 class Employee extends Person {
-	constructor(name,age,jobTitle){
-		super(name,age)
+	constructor(name, age, jobTitle) {
+		super(name, age);
 		this.jobTitle = jobTitle;
 	}
 
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title 
-		is ${this.jobTitle}`);
+	jobGreet() {
+		console.log(
+			`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+		);
 	}
 }
 
